ad5d75224ef212bcc7d7e65da9726c6b8e1e8e47
Dmitry <mahaev99@gmail.com>
2023-06-04 23:07:20 +0300

    Добавьте файлы проекта.

---
+++ TestBlog/TestBlog/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TestBlog.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? SecondName { get; set; }
+        
+        //relation
+        public List<Post>? Posts { get; set; }
+    }
+}
---
+++ TestBlog/TestBlog/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace TestBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; } 
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }    
+        //наследование
+        public string? ApplicationUserId { get; set; }
+        public ApplicationUser? ApplicationUser { get; set; }
+        public DateTime? CreatedDate { get; set; } = DateTime.UtcNow;
+        public string? Description  { get; set; }
+        public string? Slug { get; set; }
+    }
+}
